4facc75ab5c63fe717028734951f19db34ea019b
ariy-parallel <yes6sss1@gmail.com>
2018-01-25 20:59:21 +0900

    データの取得元を変更

--- sazae_san/views.py
+++ sazae_san/views.py
@@ -5,6 +5,7 @@ from sklearn import svm
 import urllib.request
 import datetime
 import bs4
+import re
 
 def index(request):
     template = loader.get_template('index.html')
@@ -31,31 +32,35 @@ def __get_episodes():
     return Episode.objects.order_by('-air_time')
 
 def __update():
-    response = urllib.request.urlopen('http://www.asahi-net.or.jp/~tk7m-ari/sazae_ichiran.html')
-    soup = bs4.BeautifulSoup(response, 'lxml')
-    for content in soup.body.contents[9].contents:
-        # 改行コード（<br/>）と改行タグ（\n）をスキップする
-        if not (content.string and content.string.strip()):
-            continue
-        number, date_string, hand = content.string.strip().split()
-        if hand == '休み':
-            continue
-        date = __to_date(date_string)
-        try:
-            Episode.objects.get(air_time = date)
-            continue
-        except Episode.DoesNotExist:
-          # （より前の部分のみ取得する
-          episode = Episode(air_time = date, hand = hand.split('（')[0])
-          episode.save()
+    for i in range(1991, datetime.date.today().year + 1):
+        response = urllib.request.urlopen('http://park11.wakwak.com/~hkn/data%s.htm' % i)
+        soup = bs4.BeautifulSoup(response, 'lxml')
+
+        for content in soup.body.contents[5].contents[0].split('\n'):
+            # 改行コード（<br/>）と改行タグ（\n）をスキップする
+            if not (content and content.strip()):
+                continue
+            number, date_string, hand = content.strip().split()
+            if hand == '休み':
+                continue
+            date = __to_date(date_string)
+            try:
+                Episode.objects.get(air_time = date)
+                continue
+            except Episode.DoesNotExist:
+                hand = __remove_brackets_inner(hand)
+                episode = Episode(air_time = date, hand = hand)
+
+                episode.save()
 
 def __to_date(date_string):
-    year, month, day = map(int, date_string.split('.'))
-    year = __format(year)
-    # 実在しない日付があるので、特別に修正する
-    if year == 1991 and month == 11 and day == 31:
-        return datetime.date(year, month + 1, 1)
-    return datetime.date(year, month, day)
+    date_string = __remove_brackets_inner(date_string)
+    d = datetime.datetime.strptime(date_string, "%Y年%m月%d日")
+    return datetime.date(d.year, d.month, d.day)
+
+# ()と()内の文字を削除する
+def __remove_brackets_inner(s):
+    return re.sub('\(.*\)', '', s)
 
 def __format(year):
     return int(year) + 2000 if year < 91 else int(year) + 1900
@@ -65,21 +70,21 @@ def __predict(episodes):
     target = []
     for i in range(len(episodes) - 2):
         data.append([
-            1 if episodes[i + 1].hand ==  Episode.HANDS[0][0] else 0,
-            1 if episodes[i + 1].hand ==  Episode.HANDS[1][0] else 0,
-            1 if episodes[i + 1].hand ==  Episode.HANDS[2][0] else 0,
-            1 if episodes[i + 2].hand ==  Episode.HANDS[0][0] else 0,
-            1 if episodes[i + 2].hand ==  Episode.HANDS[1][0] else 0,
-            1 if episodes[i + 2].hand ==  Episode.HANDS[2][0] else 0
+            1 if episodes[i + 1].hand == Episode.HANDS[0][0] else 0,
+            1 if episodes[i + 1].hand == Episode.HANDS[1][0] else 0,
+            1 if episodes[i + 1].hand == Episode.HANDS[2][0] else 0,
+            1 if episodes[i + 2].hand == Episode.HANDS[0][0] else 0,
+            1 if episodes[i + 2].hand == Episode.HANDS[1][0] else 0,
+            1 if episodes[i + 2].hand == Episode.HANDS[2][0] else 0
         ])
         target.append([episodes[i].hand])
     clf = svm.SVC(gamma=0.001, C=100.)
     clf.fit(data, target)
     return clf.predict([
-            1 if episodes[0].hand ==  Episode.HANDS[0][0] else 0,
-            1 if episodes[0].hand ==  Episode.HANDS[1][0] else 0,
-            1 if episodes[0].hand ==  Episode.HANDS[2][0] else 0,
-            1 if episodes[1].hand ==  Episode.HANDS[0][0] else 0,
-            1 if episodes[1].hand ==  Episode.HANDS[1][0] else 0,
-            1 if episodes[1].hand ==  Episode.HANDS[2][0] else 0
+            1 if episodes[0].hand == Episode.HANDS[0][0] else 0,
+            1 if episodes[0].hand == Episode.HANDS[1][0] else 0,
+            1 if episodes[0].hand == Episode.HANDS[2][0] else 0,
+            1 if episodes[1].hand == Episode.HANDS[0][0] else 0,
+            1 if episodes[1].hand == Episode.HANDS[1][0] else 0,
+            1 if episodes[1].hand == Episode.HANDS[2][0] else 0
       ])[0]
